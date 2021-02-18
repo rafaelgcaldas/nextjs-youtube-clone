fde6d051cf9cd2f8a64ce40bcdc6716481967464
rafaelgcaldas01 <rafael.gcaldas01@gmail.com>
2021-02-17 23:10:15 -0300

    Material-ui config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
       Clone youtube
+      <Button color="primary">Hello World</Button>
     </div>
   )
 }
