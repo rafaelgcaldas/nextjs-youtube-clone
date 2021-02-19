48588734d0607e4e823c64eb59faabaf14c7e2e6
rafaelgcaldas01 <rafael.gcaldas01@gmail.com>
2021-02-18 22:50:04 -0300

    Layout in progress

---
+++ src/components/Layout/TopBar.js
@@ -0,0 +1,46 @@
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    boxShadow: 'none',
+    zIndex: theme.zIndex.drawer + 1,
+    backgroundColor: theme.palette.background.default,
+  },
+  toolbar: {
+    minHeight: 56,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  logo: {
+    cursor: 'pointer',
+    height: 18,
+    marginLeft: theme.spacing(3),
+  },
+  search: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    height: 35,
+    width: 700,
+  },
+  input: {
+    flex: 1,
+  },
+}));
+
+function TopBar() {
+  const classes = useStyles();
+  
+  return (
+    <AppBar className={classes.root} color="default">
+      <Toolbar>
+        appbar
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default TopBar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 
+import Layout from '../components/Layout';
+
 export default function Home() {
   return (
-    <div>
+    <Layout title="Youtube">
       Clone youtube
       <Button color="primary">Hello World</Button>
-    </div>
+    </Layout>
   )
 }
