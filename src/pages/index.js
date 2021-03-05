79a4ef9c0c5ea9372018e01de9e1eb106e06f7c4
rafaelgcaldas01 <rafael.gcaldas01@gmail.com>
2021-03-04 23:03:26 -0300

    VideoCart component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,89 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 
-import Layout from '../components/Layout';
+import Layout from 'src/components/Layout';
+import VideoCard from 'src/components/VideoCard';
 
-export default function Home() {
+function Home({ data }) {
   return (
     <Layout title="Youtube">
-      Clone youtube
-      <Button color="primary">Hello World</Button>
+     <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item._id} item xl={3} lg={3} md={4} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   )
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title: "NExt.js: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER {PARTE #01}",
+      authorId: 1,
+      authorName: "Rafael Gomes",
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      title: "NExt.js: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER {PARTE #01}",
+      authorId: 1,
+      authorName: "Rafael Gomes",
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next02.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 3,
+      title: "NExt.js: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER {PARTE #01}",
+      authorId: 1,
+      authorName: "Rafael Gomes",
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 4,
+      title: "NExt.js: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER {PARTE #01}",
+      authorId: 1,
+      authorName: "Rafael Gomes",
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+  ];
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    }, // will be passed to the page component as props
+    // revalidate: 15,
+  };
+}
+
+// export async function getStaticProps() {
+//   const data = await getVideos();
+
+//   return {
+//     props: {
+//       data: JSON.parse(JSON.stringify(data)),
+//     }, // will be passed to the page component as props
+//     revalidate: 15,
+//   };
+// }
+
+export default Home;
